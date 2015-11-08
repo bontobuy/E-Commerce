d21a2a1de45e2d24afdc26540df2e8daab18b38d
Patrick Chung <kchungsemchong@gmail.com>
2015-11-08 19:14:49 +0400

    UpdateIdentityTable

---
+++ ECommerce/DataContexts/ECommerceMigrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ECommerce.DataContexts.ECommerceMigrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ECommerce.DataContexts.ECommerceDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"DataContexts\ECommerceMigrations";
+        }
+
+        protected override void Seed(ECommerce.DataContexts.ECommerceDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- ECommerce/DataContexts/IdentityDb.cs
+++ ECommerce/DataContexts/IdentityDb.cs
@@ -2,12 +2,14 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
 namespace ECommerce.DataContexts
 {
-    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    public class IdentityDb : IdentityDbContext<User>
     { 
         public IdentityDb()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -17,6 +19,17 @@ namespace ECommerce.DataContexts
         public static IdentityDb Create()
         {
             return new IdentityDb();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("User").Property(p => p.Id).HasColumnName("UserId");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRole");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogin");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaim").Property(p => p.Id).HasColumnName("UserClaimId");
+            modelBuilder.Entity<IdentityRole>().ToTable("Role").Property(p => p.Id).HasColumnName("RoleId");
+
         }
     }
 
