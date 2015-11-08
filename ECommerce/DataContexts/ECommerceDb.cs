88775c60448a0f6c32cd239852121de6f37de62d
Patrick Chung <kchungsemchong@gmail.com>
2015-11-08 18:19:21 +0400

    InitialCommit

---
+++ ECommerce/DataContexts/ECommerceDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ECommerce.Models;
+
+namespace ECommerce.DataContexts
+{
+    public class ECommerceDb:DbContext
+    {
+        public ECommerceDb()
+            : base("DefaultConnection") //Default Connection is the connection string
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ECommerce/DataContexts/IdentityDb.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.DataContexts
+{
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    { 
+        public IdentityDb()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+    }
+
+}
